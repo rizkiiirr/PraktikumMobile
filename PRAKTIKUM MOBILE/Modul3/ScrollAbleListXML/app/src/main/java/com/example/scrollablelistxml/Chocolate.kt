416b6382da0cb516fc0a6efa3d826c833f05dfd8
rizkiiirr <mr.rizkirmdhn@gmail.com>
2025-05-24 10:51:19 +0800

    Laporan Praktikum Modul 4

---
+++ PRAKTIKUM MOBILE/Modul3/ScrollAbleListXML/app/src/main/java/com/example/scrollablelistxml/Chocolate.kt
@@ -0,0 +1,10 @@
+package com.example.scrollablelistxml
+
+data class Chocolate(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val chocolateImageId: Int,
+    val btnLink: String
+)
+
